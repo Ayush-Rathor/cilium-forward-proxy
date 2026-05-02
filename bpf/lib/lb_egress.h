2b6acd8d035c391845385542112dd25fa4e09d4a
Ayush-Rathor <ayushrathor104@gmail.com>
2026-05-02 06:07:29 +0000

    lb: avoid packet header reads after map lookup

--- bpf/lib/lb_egress.h
+++ bpf/lib/lb_egress.h
@@ -29,27 +29,34 @@ lb_egress_apply_v4(struct __ctx_buff *ctx, struct iphdr *ip4)
 	struct csum_offset csum = {};
 	__be32 old_saddr;
 	__be32 new_saddr;
+	__u8 protocol;
+	int ihl;
 	int l4_off;
 	int ret;
 
-	key.src_ip = ip4->saddr;
+	/*
+	 * Important:
+	 * Copy all packet-header fields we need before calling helpers.
+	 * Do not read ip4->... after map lookup / checksum / store helpers.
+	 */
+	old_saddr = ip4->saddr;
+	protocol = ip4->protocol;
+	ihl = ipv4_hdrlen(ip4);
+
+	key.src_ip = old_saddr;
 
 	val = map_lookup_elem(&lb_egress_map, &key);
 	if (!val)
 		return CTX_ACT_OK;
 
-	old_saddr = ip4->saddr;
 	new_saddr = val->lb_ip;
-	l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
+	l4_off = ETH_HLEN + ihl;
 
-	/*
-	 * Update TCP/UDP/SCTP pseudo-header checksum before changing source IP.
-	 */
-	switch (ip4->protocol) {
+	switch (protocol) {
 	case IPPROTO_TCP:
 	case IPPROTO_UDP:
 	case IPPROTO_SCTP:
-		csum_l4_offset_and_flags(ip4->protocol, &csum);
+		csum_l4_offset_and_flags(protocol, &csum);
 
 		ret = csum_l4_replace(ctx, l4_off, &csum,
 				      old_saddr, new_saddr,
